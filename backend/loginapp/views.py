4c5c95128e20128f6dc466f30f71c2d9ca1ec110
serranos78 <gerardo07927@gmail.com>
2025-06-24 10:38:35 -0600

    Termine el backend

---
+++ backend/loginapp/views.py
@@ -0,0 +1,12 @@
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['username'] = self.user.username
+        return data
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
